edc4f875e1b0c81201e7e0ad95316dad72c09607
Mickael Bobovitch <mickael.bobovitch@epita.fr>
2023-11-11 11:42:08 +0100

    auth template

---
+++ src/pages/Portal.tsx
@@ -0,0 +1,13 @@
+import { Navigate, useLocation } from "react-router-dom";
+import { useAuth } from "../hooks/useAuth";
+
+export default function Portal({ children }) {
+    const auth = useAuth();
+    const location = useLocation();
+
+    if (auth) {
+        return <Navigate to="/" state={{ from: location }} />;
+    }
+
+    return children;
+}
---
+++ src/pages/Secured.tsx
@@ -0,0 +1,15 @@
+import { useAuth } from "../hooks/useAuth";
+import { Navigate, useLocation } from "react-router-dom";
+
+export default function Secured({ children }) {
+    const auth = useAuth();
+    const location = useLocation();
+
+    if (!auth) {
+        if (location.pathname === "/") return null;
+
+        return <Navigate to="/login" state={{ from: location }} replace />;
+    }
+
+    return children;
+}
