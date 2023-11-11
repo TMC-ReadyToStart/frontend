4b4d34c95a3d32e8f669d7aeec61462fb089dcdf
Mickael Bobovitch <mickael.bobovitch@epita.fr>
2023-11-11 12:43:53 +0100

    feat(dashboard)

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,17 @@
-import { Button } from "@/components/ui/button";
-import { Route, Routes } from "react-router-dom";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
-
-// import { useQuery } from "@tanstack/react-query";
+import Dashboard from "@/pages/Dashboard";
+import { Route, Routes, Navigate } from "react-router-dom";
 
 export default function App() {
-  return (
-    <div className="min-h-screen bg-gray-100">
-      <Routes>
-        <Route path="/" element={<Button>Click me</Button>} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
-    </div>
-  );
+    return (
+        <div className="min-h-screen bg-gray-100">
+            <Routes>
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/" element={<Dashboard />} />
+                <Route path="*" element={<Navigate to="/" />} />
+            </Routes>
+        </div>
+    );
 }
