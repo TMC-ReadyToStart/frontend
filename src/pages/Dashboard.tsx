6a7688d3c9d74b258c4cb7cd0c0f4fda2980a672
Mickael Bobovitch <mickael.bobovitch@epita.fr>
2023-11-11 17:33:53 +0100

    mcq

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -14,6 +14,9 @@ import { RecentSales } from "@/components/recent-sales";
 import { DataTable } from "@/components/data-table";
 import { Toaster } from "@/components/ui/toaster";
 import Cards from "@/components/cards";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Button } from "@/components/ui/button";
+import { Textarea } from "@/components/ui/textarea";
 
 export default function Dashboard() {
     return (
@@ -29,7 +32,7 @@ export default function Dashboard() {
             <div className="flex-1 min-h-screen p-8 pt-6 pl-24 space-y-4 bg-slate-100">
                 <div className="flex items-center justify-between space-y-2">
                     <h2 className="text-3xl font-bold tracking-tight">
-                        Dashboard
+                        Welcome back 👊
                     </h2>
                     <div className="flex items-center space-x-2">
                         {/* <CalendarDateRangePicker /> */}
@@ -180,6 +183,107 @@ export default function Dashboard() {
                     </Card>
                 </div>
                 <Cards />
+                <div className="grid gap-4 lg:grid-cols-8">
+                    <Card className="col-span-8 lg:col-span-4">
+                        <CardHeader>
+                            <CardTitle>SOME_TITLE</CardTitle>
+                        </CardHeader>
+                        <CardContent className="pl-2"></CardContent>
+                    </Card>
+                    <Card className="col-span-8 lg:col-span-4">
+                        <CardHeader>
+                            <CardTitle>Ask anything</CardTitle>
+                        </CardHeader>
+                        <CardContent>
+                            <div className="relative flex flex-col w-full px-0 overflow-hidden max-h-60 grow bg-background sm:rounded-md sm:border sm:px-0">
+                                <textarea
+                                    tabIndex={0}
+                                    rows={1}
+                                    placeholder="Send a message."
+                                    spellCheck={false}
+                                    className="min-h-[60px] w-full resize-none bg-transparent px-4 py-[1.3rem] focus-within:outline-none sm:text-sm"
+                                    // style="height: 62px !important;"
+                                ></textarea>
+                                <div className="absolute right-0 top-4 sm:right-4">
+                                    <button
+                                        className="inline-flex items-center justify-center w-8 h-8 p-0 text-sm font-medium transition-colors rounded-md shadow-md ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground hover:bg-primary/90"
+                                        type="submit"
+                                        data-state="closed"
+                                        disabled={false}
+                                    >
+                                        <svg
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            viewBox="0 0 256 256"
+                                            fill="currentColor"
+                                            className="w-4 h-4"
+                                        >
+                                            <path d="M200 32v144a8 8 0 0 1-8 8H67.31l34.35 34.34a8 8 0 0 1-11.32 11.32l-48-48a8 8 0 0 1 0-11.32l48-48a8 8 0 0 1 11.32 11.32L67.31 168H184V32a8 8 0 0 1 16 0Z"></path>
+                                        </svg>
+                                        <span className="sr-only">
+                                            Send message
+                                        </span>
+                                    </button>
+                                </div>
+                            </div>
+                        </CardContent>
+                    </Card>
+                </div>
+                <div className="grid gap-4 lg:grid-cols-8">
+                    <Card className="col-span-8 lg:col-span-4">
+                        <CardHeader>
+                            <CardTitle>Question 2</CardTitle>
+                            <CardDescription>
+                                <h2 className="pt-2 text-xl">
+                                    What is the best Hackathon in the world? 🤔
+                                </h2>
+                            </CardDescription>
+                        </CardHeader>
+                        {/* MCQ TEMPLATE */}
+                        <CardContent className="space-y-2">
+                            {Array.from(Array(4).keys()).map((i) => (
+                                <div className="flex space-x-2" key={i}>
+                                    <Checkbox id={`checkbox-${i}`} />
+                                    <label
+                                        htmlFor={`checkbox-${i}`}
+                                        className="text-sm"
+                                    >
+                                        GottaGoHack !!!
+                                    </label>
+                                </div>
+                            ))}
+
+                            <div className="flex flex-row justify-between w-full pt-8">
+                                <Button variant="outline">Back</Button>
+                                <Button>Next</Button>
+                            </div>
+                        </CardContent>
+                    </Card>
+                    <Card className="col-span-8 lg:col-span-4">
+                        <CardHeader>
+                            <CardTitle>Question 3</CardTitle>
+                            <CardDescription>
+                                <h2 className="pt-2 text-xl">
+                                    What does the function{" "}
+                                    <code className="px-1 py-0.5 text-sm font-mono text-white bg-gray-800 rounded-md">
+                                        console.log()
+                                    </code>{" "}
+                                    do? 😂
+                                </h2>
+                            </CardDescription>
+                        </CardHeader>
+                        <CardContent className="space-y-2">
+                            <Textarea
+                                className="w-full min-h-[60px]"
+                                placeholder="Write your answer here..."
+                            />
+
+                            <div className="flex flex-row justify-between w-full pt-8">
+                                <Button variant="outline">Back</Button>
+                                <Button>Next</Button>
+                            </div>
+                        </CardContent>
+                    </Card>
+                </div>
             </div>
             <Toaster />
         </div>
